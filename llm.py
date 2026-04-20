f1007ccbee21dbb7a6fc51fc2e43dabbeb510c9c
Santhosh <santhoshkottakota593@gmail.com>
2026-04-20 18:16:48 +0530

    Switched to Groq API and improved performance

--- llm.py
+++ llm.py
@@ -1,19 +1,13 @@
-from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def load_llm():
-    model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForCausalLM.from_pretrained(model_id)
-
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
+    return ChatGroq(
+        api_key=os.getenv("GROQ_API_KEY"),
+        model="llama-3.1-8b-instant",   # ← updated
         temperature=0.5,
-        max_new_tokens=700
+        max_tokens=700,
     )
-
-    llm_pipeline = HuggingFacePipeline(pipeline=pipe)
-    return ChatHuggingFace(llm=llm_pipeline)
